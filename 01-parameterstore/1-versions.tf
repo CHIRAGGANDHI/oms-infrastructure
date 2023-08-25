dd195ebcb19ea082b8d82ec4734c4a54d7f7d1d7
CHIRAGGANDHI <gandhi.ck@gmail.com>
2023-08-25 14:50:08 +0530

    Update 1-versions.tf

--- 01-parameterstore/1-versions.tf
+++ 01-parameterstore/1-versions.tf
@@ -18,6 +18,6 @@ terraform {
 #provider block
 provider "aws" {
   region = var.aws_region
-  access_key = "AKIA2DXVCI4KDH7TGV5V"  
-  secret_key = "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"
+  access_key = var.aws_accesskey
+  secret_key = var.aws_secret_key
 }
